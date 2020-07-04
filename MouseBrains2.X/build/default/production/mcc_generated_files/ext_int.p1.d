771d5cb43042d98195f651195bfb35063769be94
Sophi Kravitz <sophikravitz@gmail.com>
2020-07-04 15:46:51 -0400

    Delete ext_int.p1.d

--- MouseBrains2.X/build/default/production/mcc_generated_files/ext_int.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/mcc_generated_files/ext_int.d  \
- build/default/production/mcc_generated_files/ext_int.p1:  \
- mcc_generated_files/ext_int.c  \
-mcc_generated_files/../IR_receiver.h  \
-mcc_generated_files/ext_int.h 
